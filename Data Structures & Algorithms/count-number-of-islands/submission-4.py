7dc1225764f8b09ac00feb75d5a614ab94d3b180
Ducket <dangminhduc1912008@gmail.com>
2026-05-18 22:39:57 +0700

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        seen = set()
+        res = 0
+        row, col = len(grid), len(grid[0])
+        def dfs(grid, r, c, visit):
+            if r == row or c == col or min(r,c) < 0 or (r,c) in visit or int(grid[r][c]) == 0:
+                return
+
+            visit.add((r,c))
+            seen.add((r,c))
+            dfs(grid, r+1, c, visit)
+            dfs(grid, r-1, c, visit)
+            dfs(grid, r, c+1, visit)
+            dfs(grid, r, c-1, visit)
+            # no visit.remove((r,c)) as we dont need to find all paths but just the clust of '1'
+            return
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if int(grid[r][c]) == 1 and not (r,c) in seen:
+                    dfs(grid, r, c, seen)
+                    res += 1  
+        return res
