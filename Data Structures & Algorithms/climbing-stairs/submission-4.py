7ecbc9d1b69fe488dd3ec3634d6f3f588a8d8758
Ducket <dangminhduc1912008@gmail.com>
2026-05-23 11:00:58 +0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        def dfs(n, cache):
+            if n <= 2:
+                return n
+            if n in cache:
+                return cache[n]
+
+            cache[n] = dfs(n-1, cache) + dfs(n-2, cache)
+            return cache[n]
+
+        return dfs(n, {})
